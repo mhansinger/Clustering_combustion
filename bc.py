aedf2c9ba7c5bc976798b0522b795cc07a7439a6
eg <y.ge1222@gmail.com>
2018-04-18 13:22:45 +0200

    move clustering from plotting

--- bc.py
+++ bc.py
@@ -7,10 +7,10 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import dask.dataframe as dd
-#from os.path import join
+
 import os
 from scipy import interpolate
-from sklearn.cluster import KMeans,DBSCAN
+from sklearn.cluster import KMeans, DBSCAN
 import hdbscan
 from sklearn.decomposition import PCA
 from data_clustering import data_scaling
@@ -18,15 +18,15 @@ from data_clustering import npc
 
 
 def read_data(data_name, case='./Data'):
-
     # reads in the data as a Dask dataframe
-    data_all_dd = dd.read_csv(os.path.join(case, data_name),assume_missing=True)
+    data_all_dd = dd.read_csv(os.path.join(case, data_name), assume_missing=True)
 
     columns = ['ccx', 'ccy', 'ccz', 'C2H2', 'C2H4', 'C2H6', 'CH2CO', 'CH2O',
-               'CH3', 'CH3OH', 'CH4', 'CO', 'CO2', 'H', 'H2','H2O', 'H2O2', 'HO2',
+               'CH3', 'CH3OH', 'CH4', 'CO', 'CO2', 'H', 'H2', 'H2O', 'H2O2', 'HO2',
                'N2', 'O', 'O2', 'OH', 'T', 'f_Bilger', 'Chi', 'PV']
 
     data_df = data_all_dd[columns].compute()
+    data_df = data_df.reset_index(drop=True)
 
     data_df['non-eq'] = 0
 
@@ -49,6 +49,7 @@ def read_data(data_name, case='./Data'):
         xi = np.linspace(np.min(xArray), np.max(xArray), 5075)
         yi = np.linspace(np.min(yArray), np.max(yArray), 1000)
         zi = np.linspace(np.min(yArray), np.max(yArray), 1000)
+
     geo = geo_mesh
 
     return data_df, geo
@@ -57,8 +58,7 @@ def read_data(data_name, case='./Data'):
 ######################################
 # plot the field data
 ######################################
-def plot_contour(data_name, mesh, data_slt, method='contour', cmap='jet'):
-
+def plot_contour(data_name, mesh, data_slt, method='contour', cmap='jet', mask=pd.Series()):
     plane = data_name.split('_')[1]
     if plane == 'xy':
 
@@ -70,7 +70,7 @@ def plot_contour(data_name, mesh, data_slt, method='contour', cmap='jet'):
         maskdown = (YI < -0.009) & (XI < 0)
         tipp1 = (XI < 0) & (YI < 0.004) & (YI > 0.00375)
         tipp2 = (XI < 0) & (YI > -0.004) & (YI < -0.00375)
-        points = np.vstack((mesh.xArray,mesh.yArray)).T
+        points = np.vstack((mesh.xArray, mesh.yArray)).T
 
         field_interp = interpolate.griddata(points, data_slt, (XI, YI), 'linear')
         field_interp[maskup] = np.nan
@@ -78,13 +78,18 @@ def plot_contour(data_name, mesh, data_slt, method='contour', cmap='jet'):
         field_interp[tipp1] = np.nan
         field_interp[tipp2] = np.nan
 
-        plt.figure(figsize=(25,5))
-        plt.imshow(field_interp,cmap=cmap)
+        fm = field_interp
+        if not mask.empty:
+            field_mask = interpolate.griddata(points, mask, (XI, YI), 'linear')
+            fm = np.ma.masked_where(field_mask, field_interp)
+        plt.figure(figsize=(25, 5))
+        plt.imshow(fm, cmap=cmap)
 
-        plt.yticks([0,462.5,500,537.5,1000],('0.05','D/2','0','-D/2','-0.05'))
-        plt.xticks([0,75,2*75,6*75,11*75,16*75,21*75,31*75], ('-D','0','D','5D','10D','15D','20D','30D'))
+        plt.yticks([0, 462.5, 500, 537.5, 1000], ('0.05', 'D/2', '0', '-D/2', '-0.05'))
+        plt.xticks([0, 75, 2 * 75, 6 * 75, 11 * 75, 16 * 75, 21 * 75, 31 * 75],
+                   ('-D', '0', 'D', '5D', '10D', '15D', '20D', '30D'))
 
-        plt.title( method + ':' + data_slt.name)
+        plt.title(method + ':' + data_slt.name)
         plt.xlabel('x-Axis')
         plt.ylabel('y-Axis')
         plt.colorbar(label=method)
@@ -92,12 +97,16 @@ def plot_contour(data_name, mesh, data_slt, method='contour', cmap='jet'):
     elif plane == 'yz':
         # for Y-Z plane data
         YI, ZI = np.meshgrid(mesh.yi, mesh.zi)
-        points = np.vstack((mesh.yArray,mesh.zArray)).T
+        points = np.vstack((mesh.yArray, mesh.zArray)).T
 
         field_interp = interpolate.griddata(points, data_slt, (YI, ZI), 'linear')
 
-        plt.figure(figsize=(10,10))
-        plt.imshow(field_interp,cmap=cmap)
+        fm = field_interp
+        if not mask.empty:
+            field_mask = interpolate.griddata(points, mask, (YI, ZI), 'linear')
+            fm = np.ma.masked_where(field_mask, field_interp)
+        plt.figure(figsize=(10, 10))
+        plt.imshow(fm, cmap=cmap)
 
         plt.title(method + ':' + data_slt.name)
         plt.xlabel('x-Axis')
@@ -109,39 +118,40 @@ def plot_contour(data_name, mesh, data_slt, method='contour', cmap='jet'):
 
 
 # function to plot the scatter data
-def plot_scatter(data_df,y_sc, x_sc='f_Bilger',method=''):
-    if x_sc =='f_Bilger':
+def plot_scatter(data_df, y_sc, x_sc='f_Bilger', method=''):
+    if x_sc == 'f_Bilger':
         plt.xlabel('Mixture fraction')
     if y_sc == 'T':
         plt.ylabel('T [K]')
     else:
         plt.ylabel('Mass fraction')
 
-    ncs = len(set(data_df['label']))
+    ncs = len(set(data_df['label']).difference([-2]))
     cmp = plt.get_cmap('jet', ncs)
-    plt.scatter(data_df[x_sc], data_df[y_sc], s=0.5, c=data_df['label'],cmap=cmp)
+    plt.scatter(data_df[x_sc], data_df[y_sc], s=0.5, c=data_df['label'], cmap=cmp)
     plt.xlim([0, 0.15])
     plt.colorbar(ticks=range(ncs))
-    plt.title(method+' scatter plot ')
+    plt.title(method + ' scatter plot ')
     plt.show(block=False)
 
 
 def plot_cluster(data_name, mesh, data_df, model,
                  method='',
+                 mask=pd.Series(),
                  pca=0,
-                 drop=['ccx', 'ccy', 'ccz','label',
-                      'T', 'Chi', 'PV',
-                      'f_Bilger','non-eq','PV_norm','Chi_norm','PV_compute'
-                      ],
+                 drop=['ccx', 'ccy', 'ccz', 'label',
+                       'T', 'Chi', 'PV',
+                       'f_Bilger', 'non-eq', 'PV_norm', 'Chi_norm', 'PV_compute'
+                       ],
                  ):
     X_ = data_df.copy()
     drop = set(X_.columns).intersection(drop)
     X = X_.drop(drop, axis=1)
     X_pca = X
 
-    if pca>0:
+    if pca > 0:
         pca_model = PCA(n_components=pca)
-        X_pca=pca_model.fit_transform(X)
+        X_pca = pca_model.fit_transform(X)
         print(pca_model.explained_variance_ratio_.sum())
 
     # get the cluster labels
@@ -149,25 +159,52 @@ def plot_cluster(data_name, mesh, data_df, model,
     zz = model.labels_
     data_df['label'] = zz
 
-
-    n_clusters=len(set(zz))
+    n_clusters = len(set(zz).difference([-2]))
     cmap = plt.get_cmap('jet', n_clusters)
 
-    plot_contour(data_name, mesh, data_df['label'], cmap=cmap, method=method)
+    plot_contour(data_name, mesh, data_df['label'], cmap=cmap, method=method, mask=mask)
 
     X['label'] = zz
-    sub=pd.DataFrame()
+    sub = pd.DataFrame()
     for i in set(zz):
         data_sub = X[X['label'] == i].drop(['label'], axis=1)
-        sub[str(i)]=[npc(data_sub)[0],npc(data_sub)[1],sum(X['label']==i)]
+        sub[str(i)] = [npc(data_sub)[0], npc(data_sub)[1], sum(X['label'] == i)]
 
     plt.show(block=False)
 
     return sub
 
+def clustering(df,dsc,model):
+    drop = ['ccx', 'ccy', 'ccz', 'label',
+            'T', 'Chi', 'PV',
+            'f_Bilger', 'non-eq', 'PV_norm', 'Chi_norm', 'PV_compute'
+            ]
+    drop = set(dsc.columns).intersection(drop)
+
+    X = dsc.copy()
+    X = X.drop(drop, axis=1)
+    dm = X[df['f_Bilger'] > 0.01].copy()
+
+    model.fit(dm)
+    dm['label']=model.labels_
+    X['label']=dm['label']
+    X=X.fillna(-2)
+
+    df['label'] = X['label']
+    n_clusters = len(set(df['label']).difference([-2]))
+    cmap = plt.get_cmap('jet', n_clusters)
+
+
+    sub = pd.DataFrame()
+    for i in set(model.labels_):
+        data_sub = X[X['label'] == i].drop(['label'], axis=1)
+        sub[str(i)] = [npc(data_sub)[0], npc(data_sub)[1], sum(X['label'] == i)]
+
+
+    return df,cmap,sub
+
 
 if __name__ == '__main__':
-
     # data_name = 'plane_xy_00.csv'
     data_name = 'plane_yz_50.csv'
 
@@ -177,20 +214,58 @@ if __name__ == '__main__':
     # dsc = df.copy()
 
     # plot_field(data_name, mesh, df['CH4'])
-    field=df['Chi']
-    # plot_contour(data_name, mesh, field)
 
-    dbscan = DBSCAN(eps=0.005, min_samples=200)
-    kmeans = KMeans(n_clusters=5, random_state=42)
+    plot_contour(data_name, mesh, df['T'], mask=df['f_Bilger'] < 0.01)
 
-    sub_d = plot_cluster(data_name, mesh, df, dbscan,method='dbscan')
-    plot_scatter(df, 'CO',method='dbscan')
-    plt.scatter(df['label'], df['Chi'], s=0.5)
-    plt.show()
+
+
+
+    # sub_d = plot_cluster(data_name, mesh, df, dbscan, method='dbscan', mask=df['f_Bilger'] < 0.01)
+    # plot_scatter(df, 'CO', method='dbscan')
+    # plt.scatter(df['label'], df['Chi'], s=0.5)
+    # plt.show()
 
     # sub_k = plot_cluster(data_name, mesh, dsc, kmeans)
     # df['label']=dsc['label']
     # plot_scatter(df, 'CO',method='kmeans')
     # plt.scatter(df['label'], df['Chi'], s=0.5)
     # plt.show()
+
+    # a=df['f_Bilger']
+    # test=df[df['f_Bilger']>0.05]
+    # test=df['f_Bilger']>0.05
+    # test_masked = np.ma.masked_where(df['f_Bilger']>0.01,df['f_Bilger'])
+    # plot_contour(data_name,mesh,test_masked)
+
+    # drop = ['ccx', 'ccy', 'ccz', 'label',
+    #         'T', 'Chi', 'PV',
+    #         'f_Bilger', 'non-eq', 'PV_norm', 'Chi_norm', 'PV_compute'
+    #         ]
+    # drop = set(dsc.columns).intersection(drop)
+    #
+    # X = dsc.copy()
+    # X = X.drop(drop, axis=1)
+    # X=X.reset_index(drop=True)
+    # msk=df.reset_index(drop=True)
+    # dm=X[msk['f_Bilger'] > 0.01].copy()
+    #
+    # model = KMeans(n_clusters=20, random_state=42)
+    # # model = hdbscan.HDBSCAN(min_cluster_size=800)
+    # model.fit(dm)
+    # dm['label']=model.labels_
+    # X['label']=dm['label']
+    # X=X.fillna(-2)
+    #
+    # df=df.reset_index(drop=True)
+    # df['label'] = X['label']
+    # n_clusters = len(set(df['label']).difference([-2]))
+    # cmap = plt.get_cmap('jet', n_clusters)
+
+    # model = KMeans(n_clusters=5, random_state=42)
+    model = DBSCAN(eps=0.005, min_samples=200)
+    model = hdbscan.HDBSCAN(min_cluster_size=800)
+    df,cmap,cluster = clustering(df,dsc,model)
+
+    plot_contour(data_name, mesh, df['label'], mask=df['f_Bilger'] < 0.01,cmap=cmap)
+    plot_scatter(df, 'T', method='dbscan')
 
